63c1a6e497a70a238f64efc47265b74f6506cf31
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-10-05 11:49:50 +0200

    dynamic hello world

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(){
+    var students = arrayOf("Liese", "Britt", "Jasmien", "Hannelore", "Jens")
+
+    for (student in students){
+        printStudent(student)
+    }
     println("hello world")
+}
+
+fun printStudent (name:String){
+    println( "Hello $name!")
 }
