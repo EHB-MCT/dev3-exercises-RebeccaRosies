9549fbf09178738d8caa225e4e715cf6149de6ec
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-10-12 10:36:31 +0200

    WIProck_paper_scissors

--- session1/rock-paper-scissors/src/main/kotlin/main.kt
+++ session1/rock-paper-scissors/src/main/kotlin/main.kt
@@ -1,12 +1,23 @@
 fun main(){
     println("Make your choice:")
-    val userchoice = readLine()
-    println("You have entered this: $userchoice")
+    var userchoice = userChooses()
 
     println("The computer chose:")
-    var options = arrayOf("Rock", "Paper", "Scissors")
+    var options = arrayOf("rock", "paper", "scissors")
     var computerchoice = options[(0..2).random()]
     println(computerchoice)
 
-    
+   message(userchoice, computerchoice)
 }
+
+fun userChooses(userchoice:String?){
+    val userchoice = readLine()
+    println("You have entered this: $userchoice")
+    return userchoice:String?
+}
+
+fun message (x:String,y:String){
+    if (userchoice == computerchoice){
+        println("it's a tie!")
+        return
+    }}
