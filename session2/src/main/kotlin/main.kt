70def597aab787d153a9ffd7e89350a357cb4f50
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-10-12 12:52:25 +0200

    WIP challenge 3

--- session2/src/main/kotlin/main.kt
+++ session2/src/main/kotlin/main.kt
@@ -16,7 +16,7 @@ fun challengeOne() {
 }
 
 fun challengeTwo(){
-    println("You're stuck at work with a colleague when the apocalypse begins, will you stick together or will you go your separate ways ")
+    println("You and a stranger are running from the zombies. You manage to hide for a bit. Will you stick together or will you go your separate ways ")
     println("You aren't sure and roll the dice on it. Choose low or high. if your guess matches the dice you'll stick together")
     var Challenge2Guess = readLine()
     rollDice(Challenge2Guess)
@@ -42,8 +42,25 @@ fun rollDice(Challenge2Guess:String?){
     if (dice == Challenge2Guess ){
         println("you chose to stick together")
         println("congrats you might survive")
+        challengeThree()
     } else {
         println("you chose to go your separate ways")
         println("sorry, you're dead")
+    }
+}
+
+fun challengeThree(){
+    println("You pass by a hardware store. Choose a weapon!")
+    var weaponChoice = readLine()
+    println("You chose a $weaponChoice")
+    if(weaponChoice!!.length()<4){
+        println("you the moment you reach for the $weaponChoice a zombie emerges and starts to chase you")
+        challengeTwo()
+    } else if (weaponChoice!!.length()>5 ){
+        println("you take the $weaponChoice when a zombie emerges. Luckily you manage to kill it with your $weaponChoice")
+    }   else {
+        println("you reach for the $weaponChoice but a zombie emerges and eats your brains")
+        println("sorry you're dead")
+
     }
 }
