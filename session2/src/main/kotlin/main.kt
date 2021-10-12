8225c997b686797d6dd7c900caad51f4192cbeb1
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-10-12 11:23:15 +0200

    made first challenge

--- session2/src/main/kotlin/main.kt
+++ session2/src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main (){
-    println("test")
+    println("It's january 1st 2022, the Zombie apocalypse has just begun. Will you be able to survive?")
+    challengeOne()
+}
+
+fun challengeOne() {
+    println("How many times per week did you work out on average in 2021?")
+    var numberOfWorkouts = readLine()
+    val notEnoughWorkouts = setOf("one", "zero", "none", "once")
+    if (notEnoughWorkouts.contains(numberOfWorkouts)){
+        println("sorry, you're dead")
+    } else  {
+        println("congrats you survive")
+    }
 }
