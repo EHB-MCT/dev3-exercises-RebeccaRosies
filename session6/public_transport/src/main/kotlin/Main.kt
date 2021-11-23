b246e4686e1cfda383e0bdc0a0ad6e765706e7ef
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-11-23 11:22:49 +0100

    session6_libraryconnection_success

--- session6/public_transport/src/main/kotlin/Main.kt
+++ session6/public_transport/src/main/kotlin/Main.kt
@@ -5,11 +5,24 @@ import Credentials.*
 fun main() {
     Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
 
+    // haal klasse op maaar je hoeft niks mee te geven want zijn default settings
+    val credentials = Credentials()
+
+    // Prepare credentials
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.databaseUser
+    connectionProps["password"] = credentials.databasePassword
+
+
     // Create the connection: this will allow us to run queries on it later
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
-                "your host" +
-                ":" + "3306 (this is your port, this is de default)" + "/" +
-                Credentials.databaseName,
-        connectionProps)
+                "dt5.ehb.be" +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+       connectionProps)
+
+    println("succes!")
 }
+
+
