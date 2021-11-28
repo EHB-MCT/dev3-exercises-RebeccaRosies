aa32a34d7668c3bdbf403f16753852aa37716fdf
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-11-28 23:04:42 +0100

    added a count of subcategories and showed info on first destination

--- session5/geeky_travels/src/main/kotlin/main.kt
+++ session5/geeky_travels/src/main/kotlin/main.kt
@@ -25,21 +25,50 @@ fun main(){
     println("Hi!, Choose one of the following categories: Disney - Pixar - The Lord of the Rings - Narnia - Pirates of the Caribbean")
     val chosenCategory = readLine()
 
-    //------------------------------------------------------------------------
-    // put queries in here
-    val statement = connection.prepareStatement("SELECT * FROM TravelInfo " +
+    //-----GET THE NUMBER OF SUBCATEGORIES ----------------------------------------------------------------------
+    val statement = connection.prepareStatement("SELECT Category, subcategory FROM TravelInfo " +
             "WHERE Category = ? ")
     // Replace the var (?) without allowing other (full) queries to be entered by adding ';'
     statement.setString(1, chosenCategory)
     val result = statement.executeQuery()
 
-    // Show results -----------------------------------------------------------
+    val resultList: MutableList<String> = ArrayList()
+
+    // Show results -------------------------------------------
+    var count = 0
     while(result.next()){
-        println("type:" + result.getString("subcategory"))
+        resultList.add(result.getString("subcategory"))
+        count = resultList.count()
     }
+    println(count)
 
+    println("There are $count travel destinations for this category")
+    //----GET THE INFORMATION OF THE FIRST DESTINATION ------------------------------------------------------
+    val statement2 = connection.prepareStatement("SELECT * FROM TravelInfo " +
+            "WHERE Category = ? " +
+            "LIMIT 1")
+    // Replace the var (?) without allowing other (full) queries to be entered by adding ';'
+    statement2.setString(1, chosenCategory)
+    val result2 = statement2.executeQuery()
 
-
-
+    while(result2.next()){
+        println("this is the first option:" + result2.getString("subcategory"))
+        println("Destination:"+ result2.getString("country"))
+        println("more specifically:"+ result2.getString("city"))
+        println("The price (in euro) for this destination is only:"+ result2.getString("price_in _euro"))
+        println("The weather in this period is generally "+ result2.getString("weather"))
+        println("you will be staying for "+ result2.getString("number_of_days") + " days")
+        println("the departure date is on "+ result2.getString("departure_date") + " and the return date on "+ result2.getString("departure_date") )
+        println("a median of the reviews for this destination has a result of " + result2.getString("review") +" out of 10")
+    }
+    
+    //userInteraction 2---------------------------------------------------------
+    println("type 'next' to see the next destination, type 'previous' to see the previous destination, type 'select' to choose this destination")
+    when (readLine()){
+            "next" -> println("do next")
+            "previous" -> println("do previous")
+            "select" -> println("do select")
+            else -> println("please type: 'next', 'previous', or 'select'")
+    }
 
 }
